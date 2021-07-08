9022faef4e84996eca8dbf623d031248611c67c0
Abraklion​ <abraklion@gmail.com>
2021-07-08 22:15:48 +0500

    v1.0.2 : ред : gulpfile - поменял конфигурацию сборки таска styles

--- gulpfile.js
+++ gulpfile.js
@@ -21,7 +21,7 @@ global.$ = {
 
     paths: {
       pug: 'src/*.pug',
-      css: 'src/assets/less/style.less',
+      css: 'src/assets/less/*.less',
       js: 'src/assets/js/*.js',
       images: {
         img: 'src/assets/img/',
